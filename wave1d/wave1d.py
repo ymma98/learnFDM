c16ab0d4c9965b64b1764b947b66eac6102767e1
ymma98 <767615400@qq.com>
2022-07-03 20:34:43 +0800

    m

--- wave1d/wave1d.py
+++ wave1d/wave1d.py
@@ -1,20 +1,21 @@
 import numpy as np
 
-def swave1d(x1d, t1d, c):
+def solver_wave1d(func_i, func_v, func_f,  \
+                  c, L, dt, C, T, \
+                  user_action=None):
     """
     wave eqn 1d solver
-    :param x1d: (np.1darray) mesh points in x dir
-    :param t1d: (np.1darray) time points in time domain 
-    :param c: (float) c in wave eqn
+    u_tt = c^2 u_xx + f on (0,L]x(0,T]
+    :param func_i: (function) func_i(x)=u(x,t=0), i.e. 
+                   initial value function 
+    :param func_v: (function) func_v(x) = du(x,t=0)/dt
+    :param func_f: (function) func_f(x,t) on the r.h.s. of eqn
+    :param c: (float) constant on the r.h.s. of eqn
+    :param L: (float) solver range: (0,L]
+    :param dt: (float) time step
+    :param C: (float) Courant number
+    :param T: (float) end time, t\in (0,T]
+    :param uer_action: (bool)
     """
-    dx = x1d[1] - x1d[0]
-    Nx = dx.shape[0] - 1
-    dt = t1d[1] - t1d[0]
-    Nt = dt.shape[0] - 1
-    C = c*dt/dx
-    C2 = C**2
-    
-    for i in range(0, Nx+1):
-        
     
     
