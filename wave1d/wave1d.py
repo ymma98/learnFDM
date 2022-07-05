b23851a8e94c49cd7aecf5a4bf6a2815d7449ce6
ymma98 <767615400@qq.com>
2022-07-05 23:06:31 +0800

    m

--- wave1d/wave1d.py
+++ wave1d/wave1d.py
@@ -23,10 +23,10 @@ def solver_wave1d(func_i, func_v, func_f,  \
     :param uer_action: (bool)
     """
     Nt = int(round(T/dt))  # time space number
-    t = np.linspace(0, T, Nt+1)  # Nt+1: time node number
-    dx = c*dt/C
+    t = np.linspace(0, Nt*dt, Nt+1)  # Nt+1: time node number
+    dx = c*dt/(float(C))
     Nx = int(round(L/dx))  # domain space number
-    x = np.linspace(0, L, Nx)
+    x = np.linspace(0, L, Nx+1)
     C2 = C**2
     # make sure dx and dt are compatible with x and t
     dx = x[1] - x[0]
@@ -41,7 +41,7 @@ def solver_wave1d(func_i, func_v, func_f,  \
     u_n = np.zeros(Nx+1) # 1 time level back
     u_nm1 = np.zeros(Nx+1) # 2 time level back
 
-    import time;  t0 = time.clock()
+    import time;  t0 = time.time()
 
     # Load initial condition to u_n
     for i in range(0, Nx+1):
@@ -62,7 +62,7 @@ def solver_wave1d(func_i, func_v, func_f,  \
         user_action(u, x, t, 1)
 
     # switch vars before next step
-    u_nm1 = u_n;  u_n = u
+    u_nm1[:] = u_n;  u_n[:] = u
 
     for n in range(1, Nt):
         # update all inner points at time t[n+1]
@@ -78,14 +78,40 @@ def solver_wave1d(func_i, func_v, func_f,  \
                 break
 
         # switch var before next step
-        u_nm1 = u_n;    u_n = u
+        u_nm1[:] = u_n;    u_n[:] = u
     
-    cpu_time = time.clock() - t0
+    cpu_time = time.time() - t0
     return u, x, t, cpu_time
     
     
+def test_quadratic():
+    """
+    check u(x,t) = x*(L-x)*(1+t/2) is exactly produced
+    """
+    def u_exact(x,t):
+        return x*(L-x)*(1+t/2)
+    def I(x):
+        return u_exact(x, 0)
+    def V(x):
+        return 0.5*x*(L-x)
+    def f(x,t):
+        return 2*(1+0.5*t)*c**2
+    L = 2.5
+    c=1.5
+    C=0.75
+    Nx=6
+    dt = C*(L/Nx)/c
+    T=18
+
+    def assert_no_error(u, x, t, n):
+        u_e = u_exact(x, t[n])
+        diff = np.abs(u-u_e).max()
+        tol = 1E-13
+        assert diff < tol
     
-    
+    solver_wave1d(I, V, f,\
+                  c, L, dt, C, T, \
+                  user_action=assert_no_error)
     
 
     
